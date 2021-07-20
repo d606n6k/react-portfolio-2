451b51101742cf06fef52e90867bd6cc2c21eeba
William Lucht <aaronlucht@gmail.com>
2021-07-20 14:42:51 -0700

    testing form submission action to see if raw html form will send an email to my inbox

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -32,7 +32,12 @@ class Contact extends Component {
         <div className="row">
           <Slide left duration={1000}>
             <div className="eight columns">
-              <form action="" method="post" id="contactForm" name="contactForm">
+              <form
+                action="mailto:aaronlucht@gmail.com"
+                method="post"
+                id="contactForm"
+                name="contactForm"
+              >
                 <fieldset>
                   <div>
                     <label htmlFor="contactName">
